dd9cbda2064b58653be9c1976d289da5c3a61c22
Oral Ünal <oralunal@gmail.com>
2023-05-09 05:06:32 +0300

    v1.0.2

--- analytics-hunter-for-woocommerce.php
+++ analytics-hunter-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name:         Analytics Hunter for WooCommerce
  * Plugin URI:          https://www.analyticshunter.com
  * Description:         Grow your business with Analytics Hunter! Use this official plugin to help analyze your business.
- * Version:             1.0.0
+ * Version:             1.0.2
  * Requires at least:   6.2
  * Requires PHP:        7.4
  * Author:              Analyticshunter
--- readme.txt
+++ readme.txt
@@ -26,6 +26,9 @@ Some texts and links for support.
 
 == Changelog ==
 
+= 1.0.2 - 2023-05-09 =
+* Tweak - Initiated composer autoload.
+
 = 1.0.1 - 2023-05-09 =
 * Tweak - PHP 7.4 compatibility.
 
